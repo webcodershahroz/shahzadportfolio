d5dfc86f75d29e57a665f7c044f84eacaeafc45c
shah-web2002 <shahrozshahzad17@gmail.com>
2023-09-03 21:51:02 +0500

    updated20230309

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,59 @@
 import React from 'react'
+import { useState } from 'react';
 
 export default function Contact(props) {
+    const [name, setname] = useState("");
+    const [email, setemail] = useState("");
+    const [subject, setsubject] = useState("");
+    const [message, setmessage] = useState("");
+
+    const handleNameChange = (e) => {
+        setname(e.target.value)
+    }
+    const handleEmailChange = (e) => {
+        setemail(e.target.value)
+    }
+    const handleSubjectChange = (e) => {
+        setsubject(e.target.value)
+
+    }
+    const handleMessageChange = (e) => {
+        setmessage(e.target.value)
+
+    }
+
+
+    const saveFeedback = (e) => {
+        e.preventDefault();
+        const response = fetch('http://localhost:2000/portfolio/feedback', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ name, email, subject, message })
+        }).then((response) => response.text())
+            .then((text) => {
+                alert(text)
+                setname("")
+                setemail("")
+                setmessage("")
+                setsubject("")
+            });
+
+
+
+    }
     return (
         <>
-            <div id='contact' className={`p-5 contact contact-${props.mode.background==='dark'?'dark':'light'} `}>
+            <div id='contact' className={`p-5 contact contact-${props.mode.background === 'dark' ? 'dark' : 'light'} `}>
                 <section>
                     <h1 className="h1-responsive fw-bolder text-center">Contact us</h1>
                     <p className="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
                         a matter of hours to help you.</p>
 
+
                     <div className="row">
+
                         <div className="col-md-9 mb-md-0 mb-5">
-                            <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+                            <form id="contact-form" name="contact-form" onSubmit={saveFeedback}>
 
 
                                 <div className="row">
@@ -20,7 +62,7 @@ export default function Contact(props) {
                                     <div className="col-md-6">
                                         <div className="md-form mb-0">
                                             <label htmlFor="name" className="">Your name</label>
-                                            <input type="text" id="name" name="name" className="form-control" />
+                                            <input type="text" id="name" name="name" value={name} onChange={handleNameChange} required className="form-control" />
                                         </div>
                                     </div>
 
@@ -29,7 +71,7 @@ export default function Contact(props) {
                                     <div className="col-md-6">
                                         <div className="md-form mb-0">
                                             <label htmlFor="email" className="">Your email</label>
-                                            <input type="text" id="email" name="email" className="form-control" />
+                                            <input type="text" id="email" name="email" value={email} onChange={handleEmailChange} required className="form-control" />
                                         </div>
                                     </div>
 
@@ -42,7 +84,7 @@ export default function Contact(props) {
                                     <div className="col-md-12">
                                         <div className="md-form mb-0">
                                             <label htmlFor="subject" className="">Subject</label>
-                                            <input type="text" id="subject" name="subject" className="form-control" />
+                                            <input type="text" id="subject" name="subject" value={subject} onChange={handleSubjectChange} required className="form-control" />
                                         </div>
                                     </div>
                                 </div>
@@ -56,20 +98,21 @@ export default function Contact(props) {
 
                                         <div className="md-form">
                                             <label htmlFor="message">Your message</label>
-                                            <textarea type="text" id="message" name="message" rows="2" className="form-control md-textarea"></textarea>
+                                            <textarea type="text" id="message" name="message" value={message} onChange={handleMessageChange} required rows="2" className="form-control md-textarea"></textarea>
                                         </div>
 
                                     </div>
                                 </div>
 
-
+                                <div className="text-center text-md-left">
+                                    <button className="my-2 btn btn-primary" type='submit'>Send</button>
+                                </div>
                             </form>
 
-                            <div className="text-center text-md-left">
-                                <button className="my-2 btn btn-primary">Send</button>
-                            </div>
+
                             <div className="status"></div>
                         </div>
+
 
 
 
